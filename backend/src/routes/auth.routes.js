4d7edbd96c83c2109b6eadcb699c0910ff4d5887
Rishabhkr1802 <krrishabh1802@gmail.com>
2026-01-21 19:49:11 +0530

    Add minor changes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { signUp, login, logout } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post("/signup", signUp)
-router.post("/login", login)
+router.post("/login",  login)
 router.post("/logout", logout)
 
 export default router;
